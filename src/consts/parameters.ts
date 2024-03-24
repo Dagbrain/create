b203003c8718e27202b992a295d99cb5dc98b059
Dagbrain <dagrainion@gmail.com>
2024-03-24 11:49:33 +0000

    yellow

--- src/consts/parameters.ts
+++ src/consts/parameters.ts
@@ -2,20 +2,20 @@
 
 // Your smart contract address (available on the thirdweb dashboard)
 // For existing collections: import your existing contracts on the dashboard: https://thirdweb.com/dashboard
-export const contractConst = "0x436492DBc2E30E56FaC8F2297BD1964833c0687d";
+export const contractConst = "0x798715ED60D2563350a9F1e9f8df31920F7815c0";
 
 // The name of the chain your contract is deployed to.
 // Refer to README.md on how to specify the chain name.
-export const chainConst = "mumbai";
+export const chainConst = "avalanche";
 
 // It is IMPORTANT to provide your own API key to use the thirdweb SDK and infrastructure.
 // Please ensure that you define the correct domain for your API key from the API settings page.
 // You can get one for free at https://thirdweb.com/create-api-key
 // Learn more here: https://blog.thirdweb.com/changelog/api-keys-to-access-thirdweb-infra
-export const clientIdConst = import.meta.env.VITE_TEMPLATE_CLIENT_ID || "";
+export const clientIdConst = import.meta.env.VITE_TEMPLATE_CLIENT_ID || "dcede4966f3d968604ff6c87f5976cec";
 
 // Configure the primary color for buttons and other UI elements
-export const primaryColorConst = "blue";
+export const primaryColorConst = "orange";
 
 // Choose between "light" and "dark" mode
 export const themeConst = "dark";
